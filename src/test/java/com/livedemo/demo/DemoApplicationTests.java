880bdfa664b098db34ecfd4d56fa8c9e075f8df0
pablodeges <degespablo@gmail.com>
2026-06-11 19:25:44 +0200

    add real tests

--- src/test/java/com/livedemo/demo/DemoApplicationTests.java
+++ src/test/java/com/livedemo/demo/DemoApplicationTests.java
@@ -1,15 +1,27 @@
 package com.livedemo.demo;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.containsString;
 
 @SpringBootTest
+@AutoConfigureMockMvc // simulate HTTP request
 class DemoApplicationTests {
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    void contextLoads() {
-        // immer true
-        assertTrue(true);
+    void shouldReturnWelcomeMessage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk()) // 200 Code
+                .andExpect(content().string(containsString("Agiles Projektmanagement"))); 
     }
 }
